8a75c44941cdf64d1d05f89bc0373c4206258dcc
RaloboJah <lobojah0@gmail.com>
2025-05-26 21:20:06 +0300

    ajout de teste

---
+++ tests/test.test.js
@@ -0,0 +1,8 @@
+// tests/test.js
+import { describe, it, expect } from "vitest";
+
+describe("Test initial", () => {
+  it("Vérifie que true est vrai", () => {
+    expect(true).toBe(true);
+  });
+});
