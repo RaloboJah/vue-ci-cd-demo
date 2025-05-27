9f12a99e9039c0e087aff2c100246c6b6f9bad10
RaloboJah <lobojah0@gmail.com>
2025-05-27 08:08:04 +0300

    ajout de quelque texte

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -11,7 +11,7 @@ defineProps({
   <div class="greetings">
     <h1 class="green">{{ msg }}</h1>
     <h3>Une application de gestion des produits</h3>
-    <H4>Demo de CI/CD</H4>
+    <H4>Demo de CI/CD groupe nay</H4>
   </div>
 </template>
 
