3114c8d5a0f159916e81c24dd8ab98829d55ed72
Angelson007 <angelsonmanana@gmail.com>
2025-05-31 12:10:56 +0300

    changement de couleur

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -9,7 +9,7 @@ defineProps({
 
 <template>
   <div class="greetings">
-    <h1 class="green">{{ msg }}</h1>
+    <h1 class="blue">{{ msg }}</h1>
     <h4>Une application web de gestion des produits</h4>
   </div>
 </template>
